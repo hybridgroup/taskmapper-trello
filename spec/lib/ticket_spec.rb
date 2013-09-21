f5536276cdc061e13857ca2637e4f458c9eeccf1
Andrew Stewart <andrew@stwrt.ca>
2013-09-20 17:37:23 -0700

    Placeholders for ticket specs

--- spec/lib/ticket_spec.rb
+++ spec/lib/ticket_spec.rb
@@ -2,4 +2,26 @@ require 'spec_helper'
 
 describe TaskMapper::Provider::Trello::Ticket do
   let(:tm) { create_instance }
+  let(:ticket_class) { TaskMapper::Provider::Trello::Ticket }
+  let(:project) { tm.project "4f1ef9220dc5a4633b3e9d9b" }
+
+  describe "#tickets" do
+    context "with no arguments" do
+      it "returns all tickets"
+    end
+
+    context "with an array of IDs" do
+      it "returns the requested tickets"
+    end
+
+    context "with a hash containing an ID" do
+      it "returns the requested ticket"
+    end
+  end
+
+  describe "#ticket!" do
+    context "with a title and description" do
+      it "creates a new ticket"
+    end
+  end
 end
